717d2eb1906786d5eb229ec2161f6038a243d757
bill <cleber.flauzino@hotmail.com>
2023-01-19 22:54:13 -0300

    first commit

---
+++ src/TesteWhile.java
@@ -0,0 +1,14 @@
+//Programa que percorre um bloco de código desde que a condição especificada seja true
+
+public class TesteWhile {
+
+  public static void main(String[] args) {
+
+    int i = 5;
+
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
